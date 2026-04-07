85bc804a78ac6d741490215c6ec454087b5a63a5
azwallace30-sketch <azwallace30@gmail.com>
2026-04-07 17:12:24 -0400

    add tests

--- CSCI106.Tests/TheSvgBuilder.cs
+++ CSCI106.Tests/TheSvgBuilder.cs
@@ -10,5 +10,20 @@ namespace CSCI106.Tests
             Assert.That(svg, Contains.Substring("width=\"123\""));
             Assert.That(svg, Contains.Substring("height=\"456\""));
         }
+         [Test]
+        public void buildsRectangles()
+        {
+            var svgBuilder = SvgBuilder.New((500, 500));
+            svgBuilder.addRectangle(7, 9, 1, 2, "red", "");
+            
+            var svg = svgBuilder.Build(); 
+
+            Assert.That(svg, Contains.Substring("rect"));
+            Assert.That(svg, Contains.Substring("width=\"1\""));
+            Assert.That(svg, Contains.Substring("height=\"2\""));
+            Assert.That(svg, Contains.Substring("x=\"7\""));
+            Assert.That(svg, Contains.Substring("y=\"9\""));
+
+        }
     }
 }
